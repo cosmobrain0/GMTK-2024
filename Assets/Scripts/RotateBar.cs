a763a13106ad57fbab7a6f428621c502bf126a69
cosmobrain0 <cosmobrain@outlook.com>
2024-08-18 12:05:53 +0100

    added a rotating bar

---
+++ Assets/Scripts/RotateBar.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateBar : MonoBehaviour
+{
+    public Scalable rightEnd;
+    public Scalable leftEnd;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rightPercentage = PercentageAcross(rightEnd);
+        float leftPercentage = PercentageAcross(leftEnd);
+        float rotation = (leftPercentage - rightPercentage) * Mathf.PI / 2f;
+        transform.rotation = Quaternion.Euler(0, 0, rotation * Mathf.Rad2Deg);
+    }
+
+    float PercentageAcross(Scalable s)
+    {
+        return (s.CurrentScale - s.smallScale) / (s.largeScale - s.smallScale);
+    }
+}
