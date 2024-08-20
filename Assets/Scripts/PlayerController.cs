27283569e1c24788b7ca7e045df5fb2f4ee66f53
cosmobrain0 <cosmobrain@outlook.com>
2024-08-20 07:09:19 +0100

    kinda got sticking to platforms to work

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,36 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+struct CurrentGroundInfo
+{
+    public GameObject? ground;
+    public Vector3 previousPosition;
+
+    public CurrentGroundInfo(GameObject? ground)
+    {
+        this.ground = ground;
+        previousPosition = ground == null ? Vector3.zero : ground.transform.position;
+    }
+
+    public static GameObject? FindGround(Vector3 playerPosition, float playerHalfExtent)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(playerPosition, Vector3.down, out hit, 0.505f, ~4))
+        {
+            return hit.collider.gameObject;
+        }
+        if (Physics.Raycast(playerPosition - new Vector3(playerHalfExtent*0.99f, 0, 0), Vector3.down, out hit, 0.505f, ~4))
+        {
+            return hit.collider.gameObject;
+        }
+        if (Physics.Raycast(playerPosition + new Vector3(playerHalfExtent*0.99f, 0, 0), Vector3.down, out hit, 0.505f, ~4))
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
+}
+
 public class PlayerController : MonoBehaviour
 {
     CharacterController characterController;
@@ -24,12 +54,15 @@ public class PlayerController : MonoBehaviour
     Vector2 velocity;
     Goal goal;
 
+    CurrentGroundInfo groundInfo;
+
     Vector2 pushVelocity;
     public float pushDragCoefficient;
 
     // Start is called before the first frame update
     void Start()
     {
+        Physics.autoSyncTransforms = true;
         previousPosition = transform.position;
         lastTimeOnGround = null;
         jumpedSinceLastTimeOnGround = false;
@@ -40,6 +73,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Yaaaaay!");
         };
+
+        groundInfo = new CurrentGroundInfo(CurrentGroundInfo.FindGround(transform.position, transform.lossyScale.x/2f));
     }
 
     // Update is called once per frame
@@ -48,6 +83,7 @@ public class PlayerController : MonoBehaviour
         if (goal.LevelCompleted) return;
         if ((transform.position-previousPosition).magnitude >= teleportDistance)
         {
+            Debug.Log("This is causing a respawn!");
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         previousPosition = transform.position;
@@ -62,14 +98,30 @@ public class PlayerController : MonoBehaviour
             pushVelocity.x = 0f;
         }
         velocity.x = Input.GetAxis("Horizontal") * horizontalSpeed;
+        velocity += accelaration * Time.deltaTime;
         if (characterController.isGrounded)
         {
+            GameObject? currentGround = CurrentGroundInfo.FindGround(transform.position, transform.lossyScale.x/2f);
+            if (currentGround != null)
+            {
+                if (currentGround == groundInfo.ground)
+                {
+                    Vector3 offset = currentGround.transform.position - groundInfo.previousPosition;
+                    transform.Translate(new Vector2(offset.x, offset.y));
+                    groundInfo.previousPosition = currentGround.transform.position;
+                }
+                else
+                {
+                    groundInfo = new CurrentGroundInfo(currentGround);
+                }
+            }
+
             jumpedSinceLastTimeOnGround = false;
             lastTimeOnGround = DateTime.Now;
-            velocity.y = Mathf.Max(velocity.y, 0);
+            velocity.y = Mathf.Max(velocity.y, -0.1f);
             pushVelocity.y = Mathf.Max(pushVelocity.y, 0);
         }
-        velocity += accelaration * Time.deltaTime;
+        else groundInfo.ground = null;
 
         if (CanJump())
         {
